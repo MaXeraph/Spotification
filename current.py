5913763f8e30f8ad9c6fef9071797ddcc2fb1fb9
Max <33969759+MaXeraph@users.noreply.github.com>
2018-08-22 10:20:50 +0700

    Add files via upload

--- current.py
+++ current.py
@@ -8,6 +8,8 @@
 import spotipy
 import spotipy.util as util
 import json
+import pync
+import os
 
 CLIENT_ID = '0653e348f6134e17afe2533c3307bb48'
 CLIENT_SECRET = '147ceb6e7e3b4139aac65bfdebbf9557'
@@ -15,16 +17,28 @@ USER = '12180777012'
 DISPLAY = 'Max Pham'
 scope = 'user-read-playback-state' #https://developer.spotify.com/web-api/using-scopes/
 
+def notify(device: str, title: str, artist: str):
+    pync.notify("Playing: %s by %s | %s" % (title, artist, device),
+                 title='Spotify',
+                 activate='com.spotify.client',
+                 group=os.getpid(),
+                 appIcon='https://i.imgur.com/zmKJPEk.png')
+
 def current_user_playing_track(spot):
         ''' Get information about the current users currently playing track.
         '''
         return spot._get('me/player/currently-playing')
 
+def current_user_playing(spot):
+        ''' Get information about the current users currently playing.
+        '''
+        return spot._get('me/player')
+
+
 ###### AUTH
 # token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 # cache_token = token.get_access_token()
 # spotify = spotipy.Spotify(cache_token) # use for not-so-private data
-
 
 token = util.prompt_for_user_token(USER,scope,client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri='http://localhost/')
 spotify = spotipy.Spotify(auth=token)
@@ -32,22 +46,21 @@ spotify = spotipy.Spotify(auth=token)
 ###### COLLECTION
 
 ### Currently playing title
-data = current_user_playing_track(spotify)
+data, current_player  = current_user_playing_track(spotify), current_user_playing(spotify)
 parsed = json.dumps(data, indent=2) # read the whole thing
-current_title, current_artist = '', ''
+# print(parsed)
+
+current_title, current_artist, current_device = '', '', ''
 
 try:
     while True:
-        data = current_user_playing_track(spotify)
-        if current_title != data["item"]['name']:
+        data, current_player  = current_user_playing_track(spotify), current_user_playing(spotify)
+
+        if current_title != data["item"]['name'] or current_device != current_player['device']['name']:
+            current_device = current_player['device']['name']
             current_title = data["item"]['name']
             current_artist = data["item"]["artists"][0]["name"]
-            print("Playing: %s | %s" % (current_title, current_artist))
+
+            notify(current_device, current_title, current_artist)
 except KeyboardInterrupt:
     pass
-
-### Display playlist - doesnt work
-# for i, playlist in enumerate(results1['items']):
-#     if playlist['owner']['display_name'] == DISPLAY:
-#     #     print(playlist['name'].encode('utf-8'))
-#         print("%d %s" %(i, playlist['name'].encode('utf-8', 'replace')))
